ca5f60a893123f2856216df41ab24e226f15cac3
Shane Lillie <ignignokterr@gmail.com>
2018-01-27 11:21:39 -0800

    Hurr durr goin home

--- Assets/Scripts/ggj2018/BaseAttributes.cs
+++ Assets/Scripts/ggj2018/BaseAttributes.cs
@@ -8,5 +8,15 @@ namespace ggj2018.ggj2018
         private float _speedModifier;
 
         public float SpeedModifier => _speedModifier;
+
+        [SerializeField]
+        private float _turnSpeedModifier;
+
+        public float TurnSpeedModifier => _turnSpeedModifier;
+
+        [SerializeField]
+        private float _pitchSpeedModifier;
+
+        public float PitchSpeedModifier => _pitchSpeedModifier;
     }
 }
--- Assets/Scripts/ggj2018/PlayerController.cs
+++ Assets/Scripts/ggj2018/PlayerController.cs
@@ -8,9 +8,19 @@ namespace ggj2018.ggj2018
     public sealed class PlayerController : MonoBehavior
     {
         [SerializeField]
-        private float baseSpeed = 10.0f;
+        private float _baseSpeed = 10.0f;
 
         [SerializeField]
+        private float _baseTurnSpeed = 5.0f;
+
+        [SerializeField]
+        private float _basePitchSpeed = 5.0f;
+
+        [SerializeField]
+        private GameObject _model;
+
+        [SerializeField]
+        [ReadOnly]
         private BaseAttributes _attributes = new BaseAttributes();
 
         public BaseAttributes Attributes { get { return _attributes; } set { _attributes = value ?? new BaseAttributes(); } }
@@ -20,15 +30,18 @@ namespace ggj2018.ggj2018
         {
             float dt = Time.deltaTime;
 
-            Vector3 axes = InputManager.Instance.GetAxes();
+            Vector3 inputAxes = InputManager.Instance.GetAxes();
 
-            float zrot = Mathf.Clamp(axes.x, -1.0f, 1.0f) * dt;
-            float xrot = Mathf.Clamp(axes.y, -1.0f, 1.0f) * dt;
+            Vector3 rotation = _model.transform.rotation.eulerAngles;
+            rotation.x = Mathf.Clamp(rotation.x + inputAxes.x * dt, -45.0f, 45.0f);
+            rotation.z = Mathf.Clamp(rotation.z + inputAxes.y * dt, -45.0f, 45.0f);
 
-            float speed = baseSpeed + _attributes.SpeedModifier * dt;
+            float speed = _baseSpeed + _attributes.SpeedModifier * dt;
+            float turnSpeed = _baseTurnSpeed + _attributes.TurnSpeedModifier * dt;
+            float pitchSpeed = _basePitchSpeed + _attributes.PitchSpeedModifier * dt;
 
-            transform.rotation = Quaternion.Euler(xrot, 0.0f, zrot);
-            transform.position += new Vector3(axes.x, axes.y, speed);
+            _model.transform.rotation = Quaternion.Euler(rotation.x, 0.0f, rotation.y);
+            transform.position += new Vector3(turnSpeed, pitchSpeed, speed);
         }
 #endregion
     }
