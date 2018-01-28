936131f0b205fc3771575cdf364f2c3dd62e9c27
Shane Lillie <ignignokterr@gmail.com>
2018-01-27 23:14:44 -0800

    Let's try this

--- Assets/Scripts/ggj2018/SpawnPoint.cs
+++ Assets/Scripts/ggj2018/SpawnPoint.cs
@@ -1,8 +1,12 @@
 ﻿using ggj2018.Core.Util;
 
+using UnityEngine;
+
 namespace ggj2018.ggj2018
 {
     public class SpawnPoint : MonoBehavior
     {
+        [SerializeField]
+        private bool _isHawkSpawn;
     }
 }
