9c91e3442f26160d114971fcdc6008eb19a0ce3f
Shane Lillie <ignignokterr@gmail.com>
2018-01-28 16:41:06 -0800

    World boundaries instead of min/max heights (no walls tho)

--- Assets/Scripts/ggj2018/WorldBoundary.cs
+++ Assets/Scripts/ggj2018/WorldBoundary.cs
@@ -20,8 +20,32 @@ namespace ggj2018.ggj2018
 
         public bool IsVertical => BoundaryType.Ground == _boundaryType || BoundaryType.Sky == _boundaryType;
 
-        public bool Collision(Collider other)
+        private BoxCollider _collider;
+
+#region Unity Lifecycle
+        private void Awake()
         {
+            _collider = GetComponent<BoxCollider>();
+        }
+#endregion
+
+        public bool Collision(IPlayer player, Collider other)
+        {
+            Vector3 position = player.GameObject.transform.position;
+            switch(Type)
+            {
+            case BoundaryType.Ground:
+                position.y = transform.position.y + _collider.size.y + 2.0f;
+                break;
+            case BoundaryType.Sky:
+                position.y = transform.position.y - _collider.size.y - 2.0f;
+                break;
+            case BoundaryType.Wall:
+                // TODO
+                break;
+            }
+            player.GameObject.transform.position = position;
+
             return true;
         }
     }
