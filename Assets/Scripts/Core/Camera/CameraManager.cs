3287e19254e4697dcb61465d779f8341b855a40c
Shane Lillie <ignignokterr@gmail.com>
2018-01-30 00:01:17 -0800

    Better viewport resizing

--- Assets/Scripts/Core/Camera/CameraManager.cs
+++ Assets/Scripts/Core/Camera/CameraManager.cs
@@ -37,13 +37,6 @@ namespace ggj2018.Core.Camera
         {
             Debug.Log($"Spawning {count} viewers...");
 
-            int gridSizeX = Mathf.CeilToInt(Mathf.Sqrt(count));
-            int gridSizeY = gridSizeX;
-
-// TODO: cut off unused rows
-            /*int extra = (gridSizeX * gridSizeY) - count;
-            gridSizeY = extra / gridSizeY;*/
-
             for(int i=0; i<count; ++i) {
                 BaseViewer viewer = Instantiate(_viewerPrefab, _viewerContainer.transform);
                 viewer.name = $"Viewer P{i}";
@@ -53,16 +46,7 @@ namespace ggj2018.Core.Camera
                 _viewers.Add(viewer);
             }
 
-// TODO: this maybe should go in ResizeViewports() ?
-            float viewportWidth = (1.0f / gridSizeX);
-            float viewportHeight = (1.0f / gridSizeY);
-
-            for(int y=0; y<gridSizeY; ++y) {
-                for(int x=0; x<gridSizeX; ++x) {
-                    int viewerIdx = (gridSizeX * y) + x;
-                    _viewers[viewerIdx].SetViewport(x, gridSizeY - 1 - y, viewportWidth, viewportHeight);
-                }
-            }
+            ResizeViewports();
         }
 
         public BaseViewer GetViewer(int i)
@@ -77,20 +61,32 @@ namespace ggj2018.Core.Camera
 
         public void ResizeViewports()
         {
-            Debug.Log("Resizing viewports...");
-
             // TODO: probably a better way to do this...
-            List<int> activeViewers = new List<int>();
+            var activeViewers = new List<int>();
             for(int i=0; i<_viewers.Count; ++i) {
                 if(_viewers[i].gameObject.activeInHierarchy) {
                     activeViewers.Add(i);
                 }
             }
 
-            if(1 == activeViewers.Count) {
-                _viewers[activeViewers[0]].SetViewport(0, 0, 1.0f, 1.0f);
-            } else {
-                // TODO
+            Debug.Log($"Resizing {_viewers.Count} viewports, found {activeViewers.Count} active...");
+
+            // TODO: would be cooler if we favored wider rows rather than taller columns
+
+            int gridSizeX = Mathf.CeilToInt(Mathf.Sqrt(activeViewers.Count));
+            int gridSizeY = gridSizeX;
+
+            int extra = (gridSizeX * gridSizeY) - activeViewers.Count;
+            gridSizeY = gridSizeY - (extra / gridSizeX);
+
+            float viewportWidth = (1.0f / gridSizeX);
+            float viewportHeight = (1.0f / gridSizeY);
+
+            for(int y=0; y<gridSizeY; ++y) {
+                for(int x=0; x<gridSizeX; ++x) {
+                    int viewerIdx = (gridSizeX * y) + x;
+                    _viewers[activeViewers[viewerIdx]].SetViewport(x, gridSizeY - 1 - y, viewportWidth, viewportHeight);
+                }
             }
         }
 
