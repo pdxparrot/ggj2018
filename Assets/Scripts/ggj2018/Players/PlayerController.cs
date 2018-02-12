17110023fb7bcfea0696b3233907086aeced8291
Shane Lillie <ignignokterr@gmail.com>
2018-02-11 23:48:57 -0800

    Goal should work a little better

--- Assets/Scripts/ggj2018/Players/PlayerController.cs
+++ Assets/Scripts/ggj2018/Players/PlayerController.cs
@@ -151,13 +151,6 @@ namespace ggj2018.ggj2018.Players
                 return;
             }
 
-            // TODO: fix the goal object so that this check
-            // can go in the goal class rather than here
-            Goal goal = other.GetComponentInParent<Goal>();
-            if(null != goal && goal.Collision(_owner)) {
-                return;
-            }
-
             if(CheckPlayerCollision(other)) {
                 return;
             }
