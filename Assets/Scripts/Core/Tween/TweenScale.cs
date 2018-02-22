dac3393b02af3955f61a606f022953a9c3acf494
Shane Lillie <ignignokterr@gmail.com>
2018-02-21 22:16:11 -0800

    Forgot scale

---
+++ Assets/Scripts/Core/Tween/TweenScale.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+
+using pdxpartyparrot.Core.Util;
+
+using UnityEngine;
+
+namespace pdxpartyparrot.Core.Tween
+{
+    public class TweenScale : TweenRunner
+    {
+        [SerializeField]
+        [ReadOnly]
+        private Vector3 _from;
+
+        [SerializeField]
+        private Vector3 _to = new Vector3();
+
+#region Unity Lifecycle
+        protected override void Awake()
+        {
+            _from = transform.localScale;
+        }
+#endregion
+
+        public override void Reset()
+        {
+            transform.localScale = _from;
+        }
+
+        protected override Tweener CreateTweener()
+        {
+            return transform.DOScale(_to, Duration);
+        }
+    }
+}
