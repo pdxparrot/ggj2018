5702e8c3c3b21c38340c702a48e56f862f10477f
Shane Lillie <ignignokterr@gmail.com>
2018-02-25 22:12:59 -0800

    Cleanup

--- Assets/Scripts/ggj2018/UI/DebugVisualizer.cs
+++ Assets/Scripts/ggj2018/UI/DebugVisualizer.cs
@@ -17,16 +17,11 @@ namespace pdxpartyparrot.ggj2018.UI
         [SerializeField]
         private Text _debugBankForceText;
 
-        [SerializeField]
-        private float _offset = 2.5f;
-
         public void SetState(Player player)
         {
             _debugVelocityText.text = $"Velocity: {player.Controller.Rigidbody.velocity} m/s";
             _debugAngularVelocityText.text = $"Angular Velocity: {player.Controller.Rigidbody.angularVelocity} m/s";
-            _debugBankForceText.text = $"Bank Force: {player.Controller.BankForce}N";
-
-            transform.position = player.transform.position + (player.transform.forward * _offset);
+            _debugBankForceText.text = $"Bank Force: {player.Controller.BankForce} N";
         }
     }
 }
