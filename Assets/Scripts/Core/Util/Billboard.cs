7ff75755fd5744dc59ff0c0cb55ad63c02b0efdc
Shane Lillie <ignignokterr@gmail.com>
2018-02-25 23:22:08 -0800

    Untested billboarding script

---
+++ Assets/Scripts/Core/Util/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+
+using pdxpartyparrot.Core.Util;
+
+namespace pdxpartyparrot.Core.UI
+{
+    public class Billboard : MonoBehavior
+    {
+        [CanBeNull]
+        public UnityEngine.Camera Camera { get; set; }
+
+#region Unity Lifecycle
+        private void Update()
+        {
+            if(null != Camera) {
+                transform.forward = (Camera.transform.position - transform.position).normalized;
+            }
+        }
+#endregion
+    }
+}
